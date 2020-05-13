58c2aa69eff8c5f74a70e5bebb8d2a28e815d5fc
Thy Nguyen ( pronouns as tee: win) <thynguyen2511@gmail.com>
2020-05-12 23:58:11 -0700

    Create day12-singleNonDuplicate.py
    Review bitwise operations

---
+++ day12-singleNonDuplicate.py
@@ -0,0 +1,26 @@
+"""
+Problem:
+You are given a sorted array consisting of only integers where every element appears exactly twice, 
+except for one element which appears exactly once. Find this single element that appears only once. 
+
+Example 1:
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+Input: [3,3,7,7,10,11,11]
+Output: 10 
+
+Note: Your solution should run in O(log n) time and O(1) space.
+"""
+
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sum = nums[0]
+        for i in range(1, len(nums)):
+            sum = sum ^ nums[i]
+        return sum
+            
